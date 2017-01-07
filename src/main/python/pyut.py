cfaba51dc3ea6f4a6f00373c8553aa36a9999b9e
nova <skywalkerytx@gmail.com>
2017-01-08 00:24:41 +0800

    xgboost passed

--- src/main/python/pyut.py
+++ src/main/python/pyut.py
@@ -1,4 +1,5 @@
 import psycopg2
+import numpy as np
 
 
 def newconn():
@@ -27,3 +28,35 @@ codes = getcode()
 
 
 
+
+
+def data(datasize = None, labelnum = 0):
+
+    all = '''
+    SELECT
+        vector.vector,
+        label.vector
+    FROM
+        vector
+    INNER JOIN
+        label
+    ON
+        vector.code = label.code
+        AND
+        vector.date = label.date
+    ORDER BY
+        vector.code,vector.date asc
+    '''
+
+    limited =all+'\n LIMIT %s'
+
+    con, cur = newconn()
+
+    if datasize is not None:
+        cur.execute(limited, (datasize,))
+    else:
+        cur.execute(all)
+    res = cur.fetchall()
+    dtrain = np.asarray(list(map(lambda x: np.asarray(x[0]), res)))
+    dlabel = np.asarray(list(map(lambda x: x[1][labelnum], res)))
+    return dtrain, dlabel
--- src/main/python/xgbpredict.py
+++ src/main/python/xgbpredict.py
@@ -1,9 +1,27 @@
 import xgboost as xgb
-
+import numpy as np
 import pyut
+from sklearn.model_selection import train_test_split
+
+def param():
+    clfparam = {}
+    clfparam['booster'] = 'gbtree'
+    clfparam['silent'] = 0
+    clfparam['max_depth'] = 6
+    clfparam['subsample'] = 0.5
+    return clfparam
 
 if __name__ == '__main__':
-    (con, cur) = pyut.newconn()
-    cur.execute('select code,date,vector from vector limit 10')
-    res = cur.fetchall()
-    map(lambda x:print(x),res)
+    vectors, labels = pyut.data(labelnum=0)
+
+    tvector , evector, tlabel, elabel = train_test_split(vectors, labels, test_size=0.2,random_state=42)
+    tm = xgb.DMatrix(tvector, label=tlabel)
+    em = xgb.DMatrix(evector, label=elabel)
+    watchlist = [(em,'eval'),(tm,'train')]
+    num_round = 2
+    bst = xgb.train(param(),tm,num_round,watchlist)
+
+    preds = bst.predict(em)
+    labels = em.get_label()
+    print('error=%f' % (sum(1 for i in range(len(preds)) if int(preds[i] > 0.5) != labels[i]) / float(len(preds))))
+    print(tlabel)
