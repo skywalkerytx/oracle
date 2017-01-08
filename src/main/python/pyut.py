5dd0ec9eae350e53af2db1eb54c676b54142f4ae
nova <skywalkerytx@gmail.com>
2017-01-08 17:09:13 +0800

    test run

--- src/main/python/pyut.py
+++ src/main/python/pyut.py
@@ -1,6 +1,7 @@
 import psycopg2
 import numpy as np
 import xgboost as xgb
+from sklearn.model_selection import train_test_split
 
 
 def newconn():
@@ -30,17 +31,24 @@ codes = getcode()
 dates = getdate()
 
 def sourcefromdb(testsize=0.2,random_state=42):
-     vectors, labels = pyut.data(labelnum=0)
-
+     vectors, labels = data(labelnum=0)
      tvector, evector, tlabel, elabel = train_test_split(vectors, labels, test_size=testsize, random_state=random_state)
      tm = xgb.DMatrix(tvector, label=tlabel)
      em = xgb.DMatrix(evector, label=elabel)
      return tm,em
 
+trainingdatalocation = 'data/xgb/trainingdata.matrix'
+
+testingdatalocation = 'data/xgb/testingdata.matrix'
+
 def sourcefromfile():
-    tm = xgb.DMatrix('data/xgb/trainingdata.matrix')
-    em = xgb.DMatrix('data/xgb/testingdata.matrix')
+    tm = xgb.DMatrix(trainingdatalocation)
+    em = xgb.DMatrix(testingdatalocation)
     return tm,em
+
+def savetofile(tm,em):
+    tm.save_binary(trainingdatalocation)
+    em.save_binary(testingdatalocation)
 
 def data(datasize = None, labelnum = 0):
 
@@ -57,7 +65,7 @@ def data(datasize = None, labelnum = 0):
         AND
         vector.date = label.date
     ORDER BY
-        vector.code,vector.date asc
+        vector.code,vector.date ASC
     '''
 
     limited =all+'\n LIMIT %s'
@@ -71,4 +79,60 @@ def data(datasize = None, labelnum = 0):
     res = cur.fetchall()
     dtrain = np.asarray(list(map(lambda x: np.asarray(x[0]), res)))
     dlabel = np.asarray(list(map(lambda x: x[1][labelnum], res)))
+    cur.close()
+    con.close()
     return dtrain, dlabel
+
+
+def getdatebycode(code,col = 'vector'):
+    con,cur = newconn()
+    #could use parameter string but i don't care
+    cur.execute('''
+    SELECT
+        date
+    FROM ''' + col + '''
+
+    WHERE
+        code = %s
+    order by date asc
+    ''',(code,))
+    dates = cur.fetchall()
+    cur.close()
+    con.close()
+    return map(lambda x:x[0],dates)
+
+def vecbycodedate(code, date):
+    con,cur = newconn()
+    cur.execute('''
+    SELECT
+        vector
+    FROM
+        vector
+    WHERE
+        code = %s
+        AND
+        date = %s
+    ''', (code, date))
+    res = cur.fetchone()
+    dtrain = np.asarray(res)
+    cur.close()
+    con.close()
+    return dtrain
+
+def labelbycodedate(code,date):
+    con,cur = newconn()
+    cur.execute('''
+    SELECT
+        vector
+    FROM
+        label
+    WHERE
+        code = %s
+        AND
+        date = %s
+    ''', (code, date))
+    res = cur.fetchone()
+    label = res[0]
+    cur.close()
+    con.close()
+    return label[0]
--- src/main/python/xgbpredict.py
+++ src/main/python/xgbpredict.py
@@ -1,21 +1,18 @@
 import xgboost as xgb
-import numpy as np
 import pyut
-from sklearn.model_selection import train_test_split
 
 
 def param():
     clfparam = dict()
     clfparam['booster'] = 'gbtree'
     clfparam['silent'] = 0
-    clfparam['max_depth'] = 8
+    clfparam['max_depth'] = 6
     clfparam['subsample'] = 0.5
     return clfparam
 
 
-if __name__ == '__main__':
-    tm,em = pyut.sourcefromfile()
-
+def xgbst():
+    tm, em = pyut.sourcefromfile()
     watchlist = [(em, 'eval'), (tm, 'train')]
     num_round = 2
     bst = xgb.train(param(), tm, num_round, watchlist)
@@ -33,3 +30,58 @@ if __name__ == '__main__':
             else:
                 falsealarm += 1
     print(positive * 100.0 / detected, falsealarm * 100.0 / detected)
+    return bst
+
+def oneround(code):
+    bst = xgb.Booster(model_file='xgb.model')
+    detected = 0
+    mismatch = 0
+    wronglabel = 0
+    loss = 0
+    wincount = 0
+    dates = list(pyut.getdatebycode(code, 'vector'))
+    ld = list(pyut.getdatebycode(code, 'label'))
+    testdata = list(map(lambda date: pyut.vecbycodedate(code, date), ld))
+    for i in range(len(testdata)):
+        pred = bst.predict(xgb.DMatrix(testdata[i]))
+        daftt = dates[i + 2]
+        dt = dates[i + 1]
+        tormorrow = pyut.vecbycodedate(code, dt)[0][0]
+        dayafter = pyut.vecbycodedate(code, daftt)[0][1]
+        label = pyut.labelbycodedate(code, ld[i])
+        detected += 1
+        if pred > 0.5:
+            if label != 1:
+                mismatch += 1
+                loss += dayafter / tormorrow
+            else:
+                wincount += 1
+        if label == 1 and tormorrow * 1.03 < dayafter:
+            wronglabel += 1
+    return detected,mismatch,wronglabel,loss,wincount
+def simulation(bst):
+
+    from multiprocessing.pool import Pool
+    tp = Pool(4)
+    total = tp.map(oneround,pyut.codes)
+    detected = sum(map(lambda x:x[0],total))
+    mismatch = sum(map(lambda x:x[1],total))
+    wronglabel = sum(map(lambda x:x[2],total))
+    loss = sum(map(lambda x:x[3],total))
+    wincount = sum(map(lambda x:x[4],total))
+    print("wrong rate: %f"%(mismatch*1.0/detected))
+    print("avg loss: %f"%(loss/mismatch))
+    print("profit rate: %f"%((wincount*1.03-loss)/detected))
+    print('wrong label %d'%wronglabel)
+
+
+
+
+if __name__ == '__main__':
+    #pyut.getbycode('sh603166')
+    #bst = xgbst()
+    #bst.save_model('xgb.model')
+    # load model and data in
+    simulation(xgb.Booster(model_file='xgb.model'))
+    #print(pyut.labelbycodedate('sh600027','2016-11-11'))
+
